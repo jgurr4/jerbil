67c717eb251905b4b1ec5bc2431b0db38f539158
jgurr4 <jared@gurrfamily.com>
2022-01-09 19:35:17 -0700

    Forced functional tests to be ordered.

--- src/test/functional/java/com/ple/jerbil/BridgeTests.java
+++ src/test/functional/java/com/ple/jerbil/BridgeTests.java
@@ -3,13 +3,17 @@ package com.ple.jerbil;
 import com.ple.jerbil.data.DataGlobal;
 import com.ple.jerbil.data.bridge.MariadbR2dbcBridge;
 import com.ple.jerbil.testcommon.ConfigProps;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.mariadb.r2dbc.api.MariadbResult;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.util.Properties;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BridgeTests {
 
   public BridgeTests() {
@@ -17,8 +21,18 @@ public class BridgeTests {
     DataGlobal.bridge = MariadbR2dbcBridge.make(props.getProperty("driver"), props.getProperty("host"), Integer.parseInt(props.getProperty("port")), props.getProperty("user"), props.getProperty("password"));
   }
 
+  @Test
+  @Order(1)
+  void dropDbTest() {
+    final Properties props = ConfigProps.getProperties();
+    DataGlobal.bridge = MariadbR2dbcBridge.make(props.getProperty("driver"), props.getProperty("host"), Integer.parseInt(props.getProperty("port")), props.getProperty("user"), props.getProperty("password"));
+    final Flux<MariadbResult> result = DataGlobal.bridge.execute("drop database test");
+    result.subscribe();
+  }
+
   //TODO: Make it so the tests will drop the database and recreate it every time.
   @Test
+  @Order(2)
   void createDbTest() {
     final Flux<MariadbResult> result = DataGlobal.bridge.execute("create database test");
     result.subscribe();
@@ -26,6 +40,7 @@ public class BridgeTests {
   }
 
   @Test
+  @Order(3)
   void createMultipleTablesTest() {
     final Flux<MariadbResult> result = DataGlobal.bridge.execute("""
         use test;
@@ -56,7 +71,18 @@ public class BridgeTests {
     result.doOnNext(mariadbResult -> System.out.println("Statement successful!")).subscribe();
   }
 
+  //FIXME: Currently this says "MonoSingle" instead of "1 row updated".
   @Test
+  @Order(4)
+  void insertRowTest() {
+    final Flux<MariadbResult> result = DataGlobal.bridge.execute("use test; insert into item values (0, 'fire sword', 'weapon', 200), (0, 'robe', 'armor', 300)");
+    result.map(MariadbResult::getRowsUpdated)
+      .doOnNext(System.out::println)
+      .subscribe();
+  }
+
+  @Test
+  @Order(5)
   void selectQueryTest() {
     final Flux<MariadbResult> result = DataGlobal.bridge.execute("use test; select itemId, name, type, price from item;");
     result.flatMap(mariadbResult -> mariadbResult.map((row, rowMetadata) -> String.format( "%d | %s | %s | %d",
@@ -77,15 +103,6 @@ public class BridgeTests {
       System.out.println(item_entry);
     }
 */
-  }
-
-  //FIXME: Currently this says "MonoSingle" instead of "1 row updated".
-  @Test
-  void insertRowTest() {
-    final Flux<MariadbResult> result = DataGlobal.bridge.execute("use test; insert into item values (0, 'fire sword', 'weapon', 200)");
-    result.map(MariadbResult::getRowsUpdated)
-      .doOnNext(System.out::println)
-      .subscribe();
   }
 
 }
